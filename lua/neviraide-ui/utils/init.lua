13c7cf1374c71db690490df3831a95e8a1a84e08
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-08-28 13:58:18 +0900

    clean: (theme changer) remove unneeded

--- lua/neviraide-ui/utils/init.lua
+++ lua/neviraide-ui/utils/init.lua
@@ -18,30 +18,8 @@ function M.replace_word(old, new)
 end
 
 M.dirLookup = function()
-  -- local default_themes = vim.fn.readdir(
-  --   '$HOME/Study/nvim_plugins/neviraide-ui.nvim/lua/neviraide-ui/themes/colorschemes'
-  -- )
-  -- vim.fn.readdir(vim.fn.stdpath('data') .. '/lazy/base46/lua/base46/themes')
-  -- for index, theme in ipairs(default_themes) do
-  --   default_themes[index] = theme:match('(.+)%..+')
-  -- end
-
-  local fn = vim.fn
-  -- return default_themes
-  local default_themes = vim.fn.readdir(
-    vim.fn.stdpath('data') .. '/lazy/UI/lua/neviraide-ui/themes/colorschemes'
-  )
-
-  local custom_themes = vim.loop.fs_stat(
-    '$HOME/Study/nvim_plugins/neviraide-ui.nvim/lua/neviraide-ui/themes/colorschemes'
-  )
-
-  if custom_themes and custom_themes.type == 'directory' then
-    local themes_tb = fn.readdir(fn.stdpath('config') .. '/lua/custom/themes')
-    for _, value in ipairs(themes_tb) do
-      default_themes[#default_themes + 1] = value
-    end
-  end
+  local path = '/lazy/UI/lua/neviraide-ui/themes/colorschemes'
+  local default_themes = vim.fn.readdir(vim.fn.stdpath('data') .. path)
 
   for index, theme in ipairs(default_themes) do
     default_themes[index] = theme:match('(.+)%..+')
@@ -50,22 +28,23 @@ M.dirLookup = function()
   return default_themes
 end
 
-M.dirLookup1 = function()
-  local themes_dir =
-    '$HOME/Study/nvim_plugins/neviraide-ui.nvim/lua/neviraide-ui/themes/colorschemes'
-  local list = {}
-  local p = io.popen(
-    'find "'
-      .. themes_dir
-      .. [[" -type f -iname "*.lua" -execdir sh -c 'printf "%s\n" "${0%.*}"' {} ';']]
-  )
-  if p ~= nil then
-    for file in p:lines() do
-      table.insert(list, file)
-    end
-    p:close()
-  end
-  return list
-end
+-- for local working with plugin
+-- M.dirLookup1 = function()
+--   local themes_dir =
+--     '$HOME/Study/nvim_plugins/neviraide-ui.nvim/lua/neviraide-ui/themes/colorschemes'
+--   local list = {}
+--   local p = io.popen(
+--     'find "'
+--       .. themes_dir
+--       .. [[" -type f -iname "*.lua" -execdir sh -c 'printf "%s\n" "${0%.*}"' {} ';']]
+--   )
+--   if p ~= nil then
+--     for file in p:lines() do
+--       table.insert(list, file)
+--     end
+--     p:close()
+--   end
+--   return list
+-- end
 
 return M
